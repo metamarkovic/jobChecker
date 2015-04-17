57c9c812fda452986212a0ea5b0b1a738d46f232
Marjeta Markovic <markovic.marjeta@gmail.com>
2015-04-17 18:33:39 +0200

    create functions, class, etc.

--- lifetimesOverTime.py
+++ lifetimesOverTime.py
@@ -4,19 +4,23 @@ import xml.etree.cElementTree as ET
 import glob
 
 
-class lifetimesOverTime():
-    population_path = './EC14-Exp-51/population'
+class LifetimesOverTime():
+    population_path = './population'
 
     def readConfig(self,population_path):
+        f = open('51' + '_' + 'lifetimes.txt','w')
         for filename in glob.glob(os.path.join(population_path, '*.vxa')):
             tree = ET.ElementTree(file=filename)
             root = tree.getroot()
-
             lifetime = root.find('Simulator').find('StopCondition').find('StopConditionValue').text
-
-            f = open('51' + '_' + 'lifetimes.txt','w')
             f.write(str(filename) + '   ' + str(lifetime) + '\n') # python will convert \n to os.linesep
-            f.close()
+        f.close()
+
 
     def __init__(self):
         self.readConfig(self.population_path)
+
+
+
+if __name__ == "__main__":
+    lot = LifetimesOverTime()
